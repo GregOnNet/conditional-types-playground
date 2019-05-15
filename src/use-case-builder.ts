1c2962b55d7dcbce2cabe3402608affc9c7eb667
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-05-15 05:57:41 +0200

    feat: exclude funciton types from class

--- src/use-case-builder.ts
+++ src/use-case-builder.ts
@@ -1,13 +1,26 @@
-type OmitMethod<T> = T extends (...args) => unknown ? never : T;
-type OmitMethods<T> = { [K in keyof T]: OmitMethod<T[K]> };
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K
+}[keyof T];
+
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never
+}[keyof T];
+
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+
+type other<T> = {
+  [K in keyof T]: Exclude<T[K], Function>
+} 
 
 export function create<T extends new (...args) => InstanceType<T>>(
   token: T,
-  defaults: Exclude<InstanceType<T>, (...args) => unknown>
+  defaults?: Partial<other<InstanceType<T>>>,
+  mocks?: Partial<FunctionProperties<InstanceType<T>>>
 ): InstanceType<T> {
   const instance = new token();
 
-  return Object.assign(instance, defaults);
+  return Object.assign(instance, defaults, mocks);
 }
 
 class Meetup {
@@ -18,4 +31,10 @@ class Meetup {
   }
 }
 
-const meetup = create(Meetup, { participants: [] });
+const meetup = create(Meetup);
+
+
+
+type otherValue = other<Meetup>
+
+type a = Exclude<keyof Meetup, (...args) => void>

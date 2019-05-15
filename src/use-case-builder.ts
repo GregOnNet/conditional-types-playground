71052428ae9ac86422e13b0d453f347c8d0cef52
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-05-15 13:09:24 +0200

    ref: clean up builder example

--- src/use-case-builder.ts
+++ src/use-case-builder.ts
@@ -9,11 +9,9 @@ type FunctionPropertyNames<T> = {
 type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
 type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
 
-type other<T> = { [K in keyof T]: Exclude<T[K], Function> };
-
 export function create<T extends new (...args: unknown[]) => InstanceType<T>>(
   token: T,
-  defaults?: Partial<other<InstanceType<T>>>,
+  defaults?: Partial<NonFunctionProperties<InstanceType<T>>>,
   mocks?: Partial<FunctionProperties<InstanceType<T>>>
 ): InstanceType<T> {
   const instance = new token();
@@ -30,7 +28,3 @@ class Meetup {
 }
 
 const meetup = create(Meetup);
-
-type otherValue = other<Meetup>;
-
-type a = Exclude<keyof Meetup, (...args: unknown[]) => void>;
